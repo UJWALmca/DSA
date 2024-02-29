55b798932d740d1b8ec2125b408bdaacb7c37ed6
UJWAL <124799607+UJWALmca@users.noreply.github.com>
2024-02-29 23:48:08 +0530

    Add files via upload

---
+++ elements_sum.java
@@ -0,0 +1,15 @@
+public class elements_sum {
+    public static void main(String[] args) {
+        
+    int sum=0;
+
+    int[] i ={1,2,3,4,5,6};
+
+    for(int j =0;j<i.length;j++){
+         sum = sum+i[j];
+    }
+
+    System.out.println(sum);
+
+}
+}
