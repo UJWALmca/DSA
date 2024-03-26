fcaeaafd7e86b0b1752dfac49980f9d461ba1944
UJWAL <124799607+UJWALmca@users.noreply.github.com>
2024-03-26 23:52:18 +0530

    Add files via upload

---
+++ LabCycle07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LabCycle07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabCycle07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
